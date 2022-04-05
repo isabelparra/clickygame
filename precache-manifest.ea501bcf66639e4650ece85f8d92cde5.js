8041aed023438cff30a8e3634a4513643f6bc0c5
isabelparra <isabeleparra@gmail.com>
2022-04-05 12:34:03 -0400

    Updates

---
+++ precache-manifest.ea501bcf66639e4650ece85f8d92cde5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55faffb4f70fdfab8e56",
+    "url": "/clickygame/static/css/main.43235cd9.chunk.css"
+  },
+  {
+    "revision": "55faffb4f70fdfab8e56",
+    "url": "/clickygame/static/js/main.55faffb4.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/clickygame/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "3909654731af7b099daf38bcd2541162",
+    "url": "/clickygame/index.html"
+  }
+];
